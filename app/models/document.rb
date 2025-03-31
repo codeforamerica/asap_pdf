723179baa8da5b59447c196eb641847b2d436c85
Leo Kacenjar <lkacenja@gmail.com>
2025-03-31 11:57:38 -0600

    Linting fix from commit to main.

--- app/models/document.rb
+++ app/models/document.rb
@@ -145,10 +145,10 @@ class Document < ApplicationRecord
 
   def inference_summary!
     if summary.nil?
-      if Rails.env.to_s != "production"
-        endpoint_url = "http://localhost:9000/2015-03-31/functions/function/invocations"
+      endpoint_url = if Rails.env.to_s != "production"
+        "http://localhost:9000/2015-03-31/functions/function/invocations"
       else
-        endpoint_url = "https://asap-pdf-document-inference-production-00d947a60db4c1e0d.7d67968.vpc-lattice-svcs.us-east-1.on.aws"
+        "https://asap-pdf-document-inference-production-00d947a60db4c1e0d.7d67968.vpc-lattice-svcs.us-east-1.on.aws"
       end
       payload = {
         model_name: "gemini-1.5-pro-latest",
