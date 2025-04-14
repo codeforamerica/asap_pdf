7cf1d3a6b357f207f0f49c391bfd7517e578339e
Leo Kacenjar <lkacenja@gmail.com>
2025-04-14 16:07:13 -0600

    Factory should create random site names and urls.

--- spec/factories/sites.rb
+++ spec/factories/sites.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :site do
-    name { "Example Site #{(0...8).map { (65 + rand(26)).chr }.join}" }
+    name { "Example Site #{(0...8).map { rand(65..90).chr }.join}" }
     location { "Example Location" }
-    primary_url { "http://#{(0...8).map { (65 + rand(26)).chr }.join}.example.com" }
+    primary_url { "http://#{(0...8).map { rand(65..90).chr }.join}.example.com" }
   end
 end
