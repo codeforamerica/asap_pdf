06ff2d70b5e03c420232e0b2a3c150dcd2ec26f4
Leo Kacenjar <lkacenja@gmail.com>
2025-04-22 08:32:29 -0600

    Clean up documentation.

--- python_components/classifier/split_for_dev.py
+++ python_components/classifier/split_for_dev.py
@@ -5,6 +5,10 @@ from pathlib import Path
 import pandas as pd
 
 
+"""
+Utility to randomly sample scraped csv files and output them into a small development set.
+"""
+
 def find_files(args: argparse.Namespace):
     for file in glob.glob(f"{args.input}/*.csv"):
         chunk_file(file, args.output_path, args.chunk_size)
@@ -17,7 +21,7 @@ def chunk_file(file: str, output_path, chunk_size: int):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Splits scraped csv files into a smaller production set.")
+    parser = argparse.ArgumentParser(description="Splits scraped csv files into a smaller development set.")
     parser.add_argument("input", help="Folder of CSVs to split.")
     parser.add_argument("--chunk-size", type=int, default=100, help="Number of results to return.")
     parser.add_argument(
