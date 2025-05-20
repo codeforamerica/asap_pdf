77be825c64b3965cf92a0ecc5bd6e2548cc496dd
Leo Kacenjar <lkacenja@gmail.com>
2025-05-20 16:24:48 -0600

    Animate preloader.

--- app/javascript/controllers/recommendation_list_controller.js
+++ app/javascript/controllers/recommendation_list_controller.js
@@ -11,22 +11,23 @@ export default class extends Controller {
         try {
             this.buttonTarget.classList.add('hidden');
             this.preloaderTarget.classList.remove('hidden')
-            const response = await fetch(`/documents/${this.documentIdValue}/update_recommendation_inference`, {
-                method: "PATCH",
-                headers: {
-                    "Content-Type": "application/json",
-                    "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').content,
-                    "Accept": "application/json"
-                },
-            })
-            if (response.ok) {
-                const jsonSummary = await response.json()
-                this.displayTarget.innerHTML = jsonSummary.html;
-                this.preloaderTarget.classList.add('hidden')
-            } else {
-                this.displayTarget.textContent = 'An error occurred getting the recommendation list for this document. Please try again later.';
-                throw new Error("Response was not OK")
-            }
+            this.application.getControllerForElementAndIdentifier(this.preloaderTarget, 'multi-stage-preloader').animate();
+            // const response = await fetch(`/documents/${this.documentIdValue}/update_recommendation_inference`, {
+            //     method: "PATCH",
+            //     headers: {
+            //         "Content-Type": "application/json",
+            //         "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').content,
+            //         "Accept": "application/json"
+            //     },
+            // })
+            // if (response.ok) {
+            //     const jsonSummary = await response.json()
+            //     this.displayTarget.innerHTML = jsonSummary.html;
+            //     this.preloaderTarget.classList.add('hidden')
+            // } else {
+            //     this.displayTarget.textContent = 'An error occurred getting the recommendation list for this document. Please try again later.';
+            //     throw new Error("Response was not OK")
+            // }
         } catch (error) {
             console.error("Error getting the recommendation list document:", error)
             this.preloaderTarget.classList.add('hidden')
