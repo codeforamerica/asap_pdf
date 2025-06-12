5f97e68079d31bb772c502e0afd1affdb9f67597
Leo Kacenjar <lkacenja@gmail.com>
2025-06-12 09:46:31 -0600

    Try running a single synchronous doc.

--- python_components/evaluation/scripts/ci_invoke_evaluation_lambda.sh
+++ python_components/evaluation/scripts/ci_invoke_evaluation_lambda.sh
@@ -4,18 +4,13 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 BRANCH_NAME=$(git branch --show-current)
 
-counter=0
-while IFS= read -r line; do
-    TMP_PAYLOAD="/tmp/lambda-payload-$$-$counter.json"
-
-    jq -n \
+PAYLOAD=$(jq -n \
        --arg eval_model "$EVALUATION_MODEL" \
        --arg inference_model "$INFERENCE_MODEL" \
        --arg evaluation_component "$EVALUATION_COMPONENT" \
-       --arg bucket "$OUTPUT_BUCKET_NAME" \
        --arg branch "$BRANCH_NAME" \
        --arg commit "$COMMIT_SHA" \
-       --argjson docs "$line"  \
+       --argjson docs "$DOC"  \
        '{
          evaluation_model: $eval_model,
          inference_model: $inference_model,
@@ -24,21 +19,14 @@ while IFS= read -r line; do
          commit_sha: $commit,
          page_limit: 7,
          output_google_sheet: true,
-         documents: [$docs]
-       }' > "$TMP_PAYLOAD"
+         documents: [$DOC]
+       }')
 
-    echo "Invoking Lambda with payload:"
-    cat $TMP_PAYLOAD
+echo $PAYLOAD
 
-    aws lambda invoke \
-      --cli-read-timeout 900 \
-      --function-name $FUNCTION_NAME \
-      --cli-binary-format raw-in-base64-out \
-      --payload "file://$TMP_PAYLOAD" \
-      "output-$counter.json" &
-
-    ((counter++))
-done < <(jq -c '.[]' $SCRIPT_DIR/../truthset.json)
-
-wait
-cat output-*.json
+aws lambda invoke \
+  --cli-read-timeout 900 \
+  --function-name $FUNCTION_NAME \
+  --cli-binary-format raw-in-base64-out \
+  --payload $PAYLOAD \
+  "output.json"
