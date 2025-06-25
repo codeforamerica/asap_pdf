0aab57d528b3bbca2f82dd4d419c4743577827f6
Leo Kacenjar <lkacenja@gmail.com>
2025-06-25 16:19:59 -0600

    Add modal content to accessibility scan.

--- python_components/accessibility_scan/scripts/local_accessibility_scan.sh
+++ python_components/accessibility_scan/scripts/local_accessibility_scan.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Name of the image locally.
-LOCAL_ACCESSIBILITY_SCAN_IMAGE='asap:accessibility_scan'
-
 # Store the directory this script is in.
 # This allows us to run the script consistently from anywhere.
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Add an inference to our test document.
+"$SCRIPT_DIR"/../../../bin/rake "documents:add_document_inference[302,exception:is_archival,True,Fee fi fo fum.]"
+
+# Name of the image locally.
+LOCAL_ACCESSIBILITY_SCAN_IMAGE="asap:accessibility_scan"
 
 # Build the image if not cached.
 docker build -t $LOCAL_ACCESSIBILITY_SCAN_IMAGE $SCRIPT_DIR/../.
