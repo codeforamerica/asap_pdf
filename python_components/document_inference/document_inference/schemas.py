b8b74d15107e8ae0775effa343e2f1890b45b77a
Leo Kacenjar <lkacenja@gmail.com>
2025-07-09 08:44:49 -0600

    Don't return the exceptions we don't care about.

--- python_components/document_inference/document_inference/schemas.py
+++ python_components/document_inference/document_inference/schemas.py
@@ -9,8 +9,6 @@ class DocumentSummarySchema(BaseModel):
 
 
 class DocumentRecommendation(BaseModel):
-    is_individualized: SkipJsonSchema[bool] = None
-    why_individualized: SkipJsonSchema[str] = None
     is_archival: bool = Field(
         description="Whether the document meets exception 1: Archived Web Content Exception"
     )
@@ -23,5 +21,4 @@ class DocumentRecommendation(BaseModel):
     why_application: str = Field(
         description="An explanation of why the document meets or does not meet exception 2: Preexisting Conventional Electronic Documents Exception"
     )
-    is_third_party: SkipJsonSchema[bool] = None
-    why_third_party: SkipJsonSchema[str] = None
+
