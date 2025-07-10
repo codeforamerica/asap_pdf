fbc4732be0cb5a289e57c8252da4c2c267af9bad
Leo Kacenjar <lkacenja@gmail.com>
2025-07-10 09:23:39 -0600

    Migrate away from custom session table.

--- config/routes.rb
+++ config/routes.rb
@@ -10,13 +10,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", :as => :rails_health_check
 
   # resource :session
-  # get "login", to: "sessions#new", as: :login
-
-  resources :dashboard, only: [:index] do
-    collection do
-      post :upload_pdf
-    end
-  end
+  # get "login", to: "sessions#new", as: :logi
 
   resources :sites do
     member do
