43ec86ffbf6c9f682cb1e127b589d561bf7480b3
Leo Kacenjar <lkacenja@gmail.com>
2025-08-11 10:53:16 -0600

    Use auto timestamps.

--- db/migrate/20250807171517_create_feedback_items.rb
+++ db/migrate/20250807171517_create_feedback_items.rb
@@ -1,12 +1,11 @@
 class CreateFeedbackItems < ActiveRecord::Migration[8.0]
   def change
     create_table :feedback_items do |t|
-      t.datetime :creation_date
-      t.datetime :modification_date
       t.text :sentiment
       t.text :comment
       t.references :document_inference, null: true, foreign_key: {on_delete: :cascade}
       t.references :user, null: true
+      t.timestamps
     end
   end
 
