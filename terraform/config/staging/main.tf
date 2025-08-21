dd44c2e23575438321cb66d64c8d743a5e028603
Leo Kacenjar <lkacenja@gmail.com>
2025-08-21 09:07:24 -0600

    Set staging Rails env to test.

--- terraform/config/staging/main.tf
+++ terraform/config/staging/main.tf
@@ -26,7 +26,7 @@ module "asap_pdf" {
   project_name = "asap-pdf"
   environment  = "staging"
   # todo change this to test.
-  rails_environment = "production"
+  rails_environment = "test"
   backend_kms_key = module.backend.kms_key
   vpc_cidr = "10.0.0.0/16"
   public_subnet_cidrs =  ["10.0.1.0/24", "10.0.2.0/24"]
