e447c95c4261edb237313920e040b397603d56d0
Leo Kacenjar <lkacenja@gmail.com>
2025-08-21 15:22:47 -0600

    Fix linting issues.

--- app/models/document.rb
+++ app/models/document.rb
@@ -227,7 +227,7 @@ class Document < ApplicationRecord
         lambda_manager = AwsLambdaManager.new(function_url: "http://localhost:9002/2015-03-31/functions/function/invocations")
         api_host = "http://host.docker.internal:3000"
       else
-        lambda_manager = AwsLambdaManager.new(function_name: "asap-pdf-document-inference-#{Rails.env.to_s}")
+        lambda_manager = AwsLambdaManager.new(function_name: "asap-pdf-document-inference-#{Rails.env}")
       end
       payload = {
         model_name: "gemini-2.0-flash",
@@ -258,7 +258,7 @@ class Document < ApplicationRecord
       lambda_manager = AwsLambdaManager.new(function_url: "http://localhost:9002/2015-03-31/functions/function/invocations")
       api_host = "http://host.docker.internal:3000"
     else
-      lambda_manager = AwsLambdaManager.new(function_name: "asap-pdf-document-inference-#{Rails.env.to_s}")
+      lambda_manager = AwsLambdaManager.new(function_name: "asap-pdf-document-inference-#{Rails.env}")
     end
     payload = {
       model_name: "gemini-2.5-pro-preview-03-25",
