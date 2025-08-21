9b2d3557f42d1256dfee81f74afc3b63ae645df8
Leo Kacenjar <lkacenja@gmail.com>
2025-08-21 09:29:42 -0600

    Try to set up an actual staging environment, leave test for tests.

--- terraform/config/staging/main.tf
+++ terraform/config/staging/main.tf
@@ -25,8 +25,7 @@ module "asap_pdf" {
   domain_name = "staging.ada.codeforamerica.ai"
   project_name = "asap-pdf"
   environment  = "staging"
-  # todo change this to test.
-  rails_environment = "test"
+  rails_environment = "staging"
   backend_kms_key = module.backend.kms_key
   vpc_cidr = "10.0.0.0/16"
   public_subnet_cidrs =  ["10.0.1.0/24", "10.0.2.0/24"]
