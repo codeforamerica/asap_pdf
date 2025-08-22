4e868f993459b07cfeebeb4c949b958c17f49c5e
Leo Kacenjar <lkacenja@gmail.com>
2025-08-22 13:42:56 -0600

    Return commented out portion.

--- python_components/evaluation/calculate_tokens.py
+++ python_components/evaluation/calculate_tokens.py
@@ -2,9 +2,8 @@ import json
 import time
 
 import pandas as pd
-
-from evaluation.utility.schema import Document
 from evaluation.utility.asap_inference import get_inference_for_document
+from evaluation.utility.schema import Document
 
 LLM_RATES = {
     "gemini-2.0-flash": {
@@ -29,27 +28,36 @@ def get_truthset(path: str) -> list:
 
 def make_inferences(truthset: list[Document]) -> None:
     inference_modes = (
-        #("gemini-2.0-flash", "summary"),
+        ("gemini-2.0-flash", "summary"),
         ("gemini-2.5-pro-preview-03-25", "exception"),
     )
     output = []
     for document in truthset:
         for inference_mode in inference_modes:
             try:
-                result = get_inference_for_document(document, *inference_mode, local_mode=True, aws_env="staging",
-                                                    page_number=7)
-                output.append({
-                    "document": document.url,
-                    "inference_mode": inference_mode[1],
-                    "inference_llm": inference_mode[0],
-                    "input_tokens": result["usage"]["input"],
-                    "output_tokens": result["usage"]["output"],
-                    "detail": json.dumps(result["usage"]),
-                    **LLM_RATES[inference_mode[0]]
-                })
+                result = get_inference_for_document(
+                    document,
+                    *inference_mode,
+                    local_mode=True,
+                    aws_env="staging",
+                    page_number=7,
+                )
+                output.append(
+                    {
+                        "document": document.url,
+                        "inference_mode": inference_mode[1],
+                        "inference_llm": inference_mode[0],
+                        "input_tokens": result["usage"]["input"],
+                        "output_tokens": result["usage"]["output"],
+                        "detail": json.dumps(result["usage"]),
+                        **LLM_RATES[inference_mode[0]],
+                    }
+                )
                 time.sleep(2)
             except Exception as e:
-                print(f"Failed {inference_mode[1]} for document {document.url} skipping...")
+                print(
+                    f"Failed {inference_mode[1]} for document {document.url} skipping..."
+                )
     df = pd.DataFrame(output)
     df["input_cost"] = df["input_tokens"] * df["input_rate"]
     df["output_cost"] = df["output_tokens"] * df["output_rate"]
