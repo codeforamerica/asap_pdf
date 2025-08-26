07faab69372c99d8b286c4d1e684ff38e16a755e
Leo Kacenjar <lkacenja@gmail.com>
2025-08-26 15:47:11 -0600

    Refactor and simplify API.

--- app/api/asap_pdf/api.rb
+++ app/api/asap_pdf/api.rb
@@ -6,152 +6,138 @@ module AsapPdf
     format :json
 
     http_basic do |email, password|
-      user = User.find_by(email: email)
-      user&.valid_password?(password)
+      @user = User.find_by(email: email)
+      @user&.valid_password?(password)
     end
 
     rescue_from ActiveRecord::RecordNotFound do |e|
-      error!({error: e.message}, 404)
+      error!({ error: e.message }, 404)
     end
 
-    desc "Return list of sites" do
-      detail "Returns a list of all sites in the system"
-      tags ["Sites"]
-      produces ["application/json"]
-      failure [[401, "Unauthorized"], [403, "Forbidden"]]
-    end
-    get "/sites" do
-      Site.all
-    end
-
-    desc "Return a specific site" do
-      detail "Returns detailed information about a specific site"
-      tags ["Sites"]
-      produces ["application/json"]
-      failure [
-        [401, "Unauthorized"],
-        [403, "Forbidden"],
-        [404, "Site not found"]
-      ]
-    end
-    params do
-      requires :id, type: Integer, desc: "Site ID"
-    end
-    get "/sites/:id" do
-      Site.find(params[:id])
-    end
-
-    desc "Discover documents for a site" do
-      detail "Creates or updates documents for a specific site based on the provided URLs and timestamps"
-      tags ["Documents"]
-      produces ["application/json"]
-      consumes ["application/json"]
-      failure [
-        [400, "Bad Request - Invalid parameters"],
-        [401, "Unauthorized"],
-        [403, "Forbidden"],
-        [404, "Site not found"]
-      ]
-      named "Create Documents"
-    end
-    params do
-      requires :id, type: Integer, desc: "Site ID"
-      requires :documents, type: Array do
-        requires :url, type: String, desc: "Document URL"
-        requires :modification_date, type: DateTime, desc: "Document's last modified timestamp"
+    resource :sites do
+      desc "Return list of sites" do
+        detail "Returns a list of all sites the user has access to."
+        tags ["Sites"]
+        produces ["application/json"]
+        failure [[401, "Unauthorized"], [403, "Forbidden"]]
+        security [{ basic_auth: [] }]
+      end
+      get do
+        @user.is_site_admin ? Site.all : [@user.site]
       end
     end
-    post "/sites/:id/documents" do
-      site = Site.find(params[:id])
-      documents = site.discover_documents!(params[:documents], true)
 
-      status 201
-      {documents: documents.map { |doc|
-        {
-          id: doc.id,
-          url: doc.url,
-          document_status: doc.document_status,
-          s3_path: doc.s3_path
-        }
-      }}
-    end
-
-    desc "Adds or updates a document inference with type" do
-      detail "Adds or updates a document inference with the provided type and document id."
-      tags ["Document Inferences"]
-      produces ["application/json"]
-      consumes ["application/json"]
-      failure [
-        [400, "Bad Request - Invalid parameters"],
-        [401, "Unauthorized"],
-        [403, "Forbidden"],
-        [404, "Site not found"]
-      ]
-    end
-    params do
-      requires :id, type: Integer, desc: "Document ID"
-      requires :inference_type, type: String, desc: "Document inference type", values: ["summary", "exception"]
-      requires :result, type: Hash, desc: "Value of document inference"
-    end
-    post "/documents/:id/inference" do
-      status 201
-      if params[:inference_type] == "summary"
-        inference = DocumentInference.create(document_id: params[:id], inference_type: "summary")
-        inference.inference_value = params[:result]["summary"]
-        inference.is_active = true
-        inference.save!
+    resource :documents do
+      desc "List documents related to site." do
+        detail "A paginated list of documents related to a site."
+        tags ["Documents"]
+        produces ["application/json"]
+        consumes ["application/json"]
+        failure [
+                  [400, "Bad Request - Invalid parameters"],
+                  [401, "Unauthorized"],
+                  [403, "Forbidden"],
+                  [404, "Site not found"]
+                ]
+        named "List documents"
       end
-      if params[:inference_type] == "exception"
-        ["individualized", "archival", "application", "third_party"].each do |type|
-          result_boolean = "is_#{type}"
-          unless params[:result][result_boolean].nil?
-            inference = DocumentInference.create(document_id: params[:id], inference_type: "exception:#{result_boolean}")
-            inference.inference_value = params[:result][result_boolean] ? "True" : "False"
-            inference.inference_confidence = params[:result]["#{result_boolean}_confidence"]
-            inference.inference_reason = params[:result]["why_#{type}"]
-            inference.is_active = true
-            inference.save!
+      params do
+        requires :id, type: Integer, desc: "Site ID"
+        optional :page, type: Integer, desc: 'Page number for pagination', default: 0
+        optional :items_per_page, type: Integer, desc: 'Items per page', default: 25
+      end
+      get "/sites/:id/documents" do
+        items_per_page = params[:items_per_page].nil? ? 25 : params[:items_per_page].to_i
+        page = params[:page].nil? ? 0 : params[:page].to_i
+        unless @user.is_site_admin || params[:id] == @user.site_id
+          { documents: [] }
+        end
+        site = Site.find(params[:id])
+        { documents: site.documents.limit(items_per_page).offset(page * items_per_page).order(id: :asc) }
+      end
+    end
+
+    resource :document_inferences do
+      desc "List document inferences for a document." do
+        detail "List LLM document inferences (summary or exception check) for a document."
+        tags ["Document Inferences"]
+        produces ["application/json"]
+        consumes ["application/json"]
+        failure [
+                  [400, "Bad Request - Invalid parameters"],
+                  [401, "Unauthorized"],
+                  [403, "Forbidden"],
+                  [404, "Site not found"]
+                ]
+      end
+      params do
+        requires :id, type: Integer, desc: "Document ID"
+      end
+      get "/documents/:id/inference" do
+        status 201
+        document = Document.find(params[:id])
+        { document_inferences: document.document_inferences.order(id: :asc) }
+      end
+
+      desc "Adds or updates a document inference with type" do
+        detail "Adds or updates a document inference with the provided type and document id."
+        tags ["Document Inferences"]
+        produces ["application/json"]
+        consumes ["application/json"]
+        failure [
+                  [400, "Bad Request - Invalid parameters"],
+                  [401, "Unauthorized"],
+                  [403, "Forbidden"],
+                  [404, "Site not found"]
+                ]
+      end
+      params do
+        requires :id, type: Integer, desc: "Document ID"
+        requires :inference_type, type: String, desc: "Document inference type", values: ["summary", "exception"]
+        optional :result, type: Hash, desc: "Value of document inference" do
+          optional "summary", type: String, desc: "For inference_type summary, generated summary"
+          optional "is_<exception type>", type: String, desc: "For inference_type exception, the exception type"
+          optional "why_<exception type>", type: String, desc: "For inference_type exception, the LLM explanation"
+        end
+      end
+      post "/documents/:id/inference" do
+        status 201
+        if params[:inference_type] == "summary"
+          inference = DocumentInference.create(document_id: params[:id], inference_type: "summary")
+          inference.inference_value = params[:result]["summary"]
+          inference.is_active = true
+          inference.save!
+        end
+        if params[:inference_type] == "exception"
+          ["individualized", "archival", "application", "third_party"].each do |type|
+            result_boolean = "is_#{type}"
+            unless params[:result][result_boolean].nil?
+              inference = DocumentInference.create(document_id: params[:id], inference_type: "exception:#{result_boolean}")
+              inference.inference_value = params[:result][result_boolean] ? "True" : "False"
+              inference.inference_confidence = params[:result]["#{result_boolean}_confidence"]
+              inference.inference_reason = params[:result]["why_#{type}"]
+              inference.is_active = true
+              inference.save!
+            end
           end
         end
       end
     end
 
     add_swagger_documentation(
+      doc_version: "1.0.0",
       mount_path: "/swagger_doc",
-      openapi_version: "3.0.1",
       info: {
         title: "ASAP PDF API",
-        description: "API for managing ASAP PDF resources and document processing",
+        description: "API for managing ASAP PDF resources and document processing. <strong>Note: Basic authentication is required for all endpoints.</strong>",
         version: "1.0.0"
       },
       tags: [
-        {name: "Sites", description: "Site management operations"},
-        {name: "Documents", description: "Document processing operations"}
+        { name: "Sites", description: "Site operations" },
+        { name: "Documents", description: "Document operations" },
+        { name: "Document Inferences", description: "Document Inference operations" }
       ],
-      components: {
-        schemas: {
-          Site: {
-            type: "object",
-            properties: {
-              id: {type: "integer", description: "Site ID"},
-              name: {type: "string", description: "Site name"},
-              location: {type: "string", description: "Site location"},
-              primary_url: {type: "string", description: "Primary URL of the site"}
-            },
-            required: ["id", "name", "location", "primary_url"]
-          },
-          Document: {
-            type: "object",
-            properties: {
-              id: {type: "integer", description: "Document ID"},
-              url: {type: "string", description: "Document URL"},
-              document_status: {type: "string", description: "Current status of the document"},
-              s3_path: {type: "string", description: "S3 storage path"}
-            },
-            required: ["id", "url", "document_status", "s3_path"]
-          }
-        }
-      }
+      models: [],
     )
   end
 end
