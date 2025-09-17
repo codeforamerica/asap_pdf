ddd7e039c5bdad6f4735daeb8ae56122f8c9c49f
Leo Kacenjar <lkacenja@gmail.com>
2025-09-17 13:05:04 -0600

    Fix up linting.

--- db/migrate/20250912151347_add_last_crawl_date.rb
+++ db/migrate/20250912151347_add_last_crawl_date.rb
@@ -6,6 +6,6 @@ class AddLastCrawlDate < ActiveRecord::Migration[8.0]
 
   def down
     remove_column :documents, :last_crawl_date
-    change_column_default :documents, :document_status, from:  Document::DOCUMENT_STATUS_ACTIVE, to: nil
+    change_column_default :documents, :document_status, from: Document::DOCUMENT_STATUS_ACTIVE, to: nil
   end
 end
