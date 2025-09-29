9c21a600b18201f2d6b530432f49eaa39bc6452f
Leo Kacenjar <lkacenja@gmail.com>
2025-09-29 15:51:50 -0600

    Fix configuration screen on staging. Make evaluation config more obvious.

--- app/controllers/configurations_controller.rb
+++ app/controllers/configurations_controller.rb
@@ -5,26 +5,41 @@ class ConfigurationsController < AuthenticatedController
 
   def initialize
     super
+    @config = {
+      "google_ai_api_key": nil,
+      "anthropic_api_key": nil,
+      "openai_api": nil,
+      "google_evaluation_service_account_credentials": nil,
+      "google_evaluation_sheet_id": nil,
+      "localstack_not_reachable": false
+    }
+
     @secret_manager = AwsLocalSecretManager.new
-    @secret_names = Rails.configuration.local_secret_names
+
+    if Rails.configuration.respond_to?(:local_secret_names)
+      @secret_names = Rails.configuration.local_secret_names
+    else
+      @config["localstack_not_reachable"] = true
+    end
   end
 
   def edit
-    @config = {
-      localstack_not_reachable: false
-    }
-    response = @secret_manager.get_secret!(@secret_names[:google_api])
-    @config["google_ai_api_key"] = response.secret_string if response.present?
-    response = @secret_manager.get_secret!(@secret_names[:anthropic_api])
-    @config["anthropic_api_key"] = response.secret_string if response.present?
-    response = @secret_manager.get_secret!(@secret_names[:openai_api])
-    @config["openai_api"] = response.secret_string if response.present?
-    response = @secret_manager.get_secret!(@secret_names[:google_eval_service_account])
-    @config["google_evaluation_service_account_credentials"] = response.secret_string if response.present?
-    response = @secret_manager.get_secret!(@secret_names[:google_eval_sheet_id])
-    @config["google_evaluation_sheet_id"] = response.secret_string if response.present?
-  rescue Seahorse::Client::NetworkingError
-    @config["localstack_not_reachable"] = true
+    unless @config["localstack_not_reachable"]
+      begin
+        response = @secret_manager.get_secret!(@secret_names[:google_api])
+        @config["google_ai_api_key"] = response.secret_string if response.present?
+        response = @secret_manager.get_secret!(@secret_names[:anthropic_api])
+        @config["anthropic_api_key"] = response.secret_string if response.present?
+        response = @secret_manager.get_secret!(@secret_names[:openai_api])
+        @config["openai_api"] = response.secret_string if response.present?
+        response = @secret_manager.get_secret!(@secret_names[:google_eval_service_account])
+        @config["google_evaluation_service_account_credentials"] = response.secret_string if response.present?
+        response = @secret_manager.get_secret!(@secret_names[:google_eval_sheet_id])
+        @config["google_evaluation_sheet_id"] = response.secret_string if response.present?
+      rescue Seahorse::Client::NetworkingError
+        @config["localstack_not_reachable"] = true
+      end
+    end
   end
 
   def update
