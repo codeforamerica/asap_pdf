08ba71bb49223747d811e0b9a94fed1433606f36
Leo Kacenjar <lkacenja@gmail.com>
2026-04-07 12:53:52 -0600

    Add thruster port and unpin tailwind.

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -26,7 +26,8 @@ module "fargate_service" {
 
   environment_variables = {
     RAILS_ENV           = var.rails_environment
-    PORT = tostring(var.container_port)
+    PORT                = tostring(var.container_port)
+    THRUSTER_HTTP_PORT  = tostring(var.container_port)
     WEB_CONCURRENCY     = "2"
     MALLOC_ARENA_MAX    = "2"
     RAILS_MAX_THREADS   = "5"
